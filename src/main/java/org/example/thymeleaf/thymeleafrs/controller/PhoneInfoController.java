9fe191b7208c75122dcfb4415e7a0214225bb7a5
farhanhunter <heryapry03@gmail.com>
2025-09-01 13:03:06 +0700

    [FEAT] : Annotation @AuthenticationPrincipal sebagai ganti authentication/principal yang diambil dari informasi akun pada JWT token

--- src/main/java/org/example/thymeleaf/thymeleafrs/controller/PhoneInfoController.java
+++ src/main/java/org/example/thymeleaf/thymeleafrs/controller/PhoneInfoController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/phoneinfo")
+@RequestMapping("/api/phone-info")
 public class PhoneInfoController {
 
     private final PhoneInfoService phoneInfoService;
